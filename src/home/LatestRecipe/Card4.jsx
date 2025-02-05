6f38b778f0510a1ad58c58f79da2d7f554e7aded
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-02-05 22:03:10 +0530

    05-02-2025

---
+++ src/home/LatestRecipe/Card4.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// const Card4 = () => {
+//   return (
+    
+//   )
+// }
+
+// export default Card4
--- src/home/LatestRecipe/Cards.jsx
+++ src/home/LatestRecipe/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from './Card';
-
+// import Card4 from './Card4';
 const Cards = () => {
   const cardData = [
     {
